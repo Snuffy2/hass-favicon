2b67353bd9574c86434b60f7b6d8fe50de817ccb
Snuffy2 <Snuffy2@users.noreply.github.com>
2025-01-30 21:57:19 -0500

    Linting cleanup

---
+++ custom_components/favicon/const.py
@@ -0,0 +1,10 @@
+"""hass-favicon Constants."""
+
+VERSION = "v10.4"
+DOMAIN = "favicon"
+
+CONF_TITLE = "title"
+CONF_ICON_PATH = "icon_path"
+
+DEFAULT_TITLE = "Home Assistant"
+DEFAULT_ICON_PATH = "/local/favicons/"
