e0663656f06d72b080e38e72f4d95ccc5cfb7bcd
Snuffy2 <Snuffy2@users.noreply.github.com>
2025-01-31 20:40:07 -0500

    Launch item color option (#4)
    * Add customization of launch icon color
    
    * Support latest version of Home Assistant
    
    * Use color selector
    
    * Use RegEx for Icon Color
    
    ---------
    
    Co-authored-by: Ming Aldrich-Gan <mingaldrichgan@gmail.com>

--- custom_components/favicon/const.py
+++ custom_components/favicon/const.py
@@ -5,6 +5,10 @@ DOMAIN = "favicon"
 
 CONF_TITLE = "title"
 CONF_ICON_PATH = "icon_path"
+CONF_ICON_COLOR = "launch_icon_color"
+
+CONF_KEYS = [CONF_TITLE, CONF_ICON_PATH, CONF_ICON_COLOR]
 
 DEFAULT_TITLE = "Home Assistant"
 DEFAULT_ICON_PATH = "/local/favicons/"
+DEFAULT_ICON_COLOR = "#18BCF2"
